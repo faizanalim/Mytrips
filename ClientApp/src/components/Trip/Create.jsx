9dac0c7128a4a4cb3523b3701805fd381e0f05ba
faizanalim <faizan.23@hotmail.com>
2024-08-24 04:36:24 +0500

    AddTrip

--- ClientApp/src/components/Trip/Create.jsx
+++ ClientApp/src/components/Trip/Create.jsx
@@ -1,18 +1,76 @@
 import React, {Component} from 'react';
+import axios from 'axios';
+
+import { Navigate } from 'react-router-dom';
 
 export class Create extends Component{
     constructor(props){
         super(props);
 
+        this.onChangeName = this.onChangeName.bind(this);
+        this.onChangeDescription = this.onChangeDescription.bind(this);
+        this.onChangeDateStarted = this.onChangeDateStarted.bind(this);
+        this.onChangeDateCompleted = this.onChangeDateCompleted.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+       
         this.state = {
             name: '',
             description: '',
-            dateStarted: null,
-            dateCompleted: null
+            dateStarted:'' ,
+            dateCompleted: '',
+            redirect: false
         }
     }
 
+    onChangeName(e){
+        this.setState({
+            name: e.target.value
+        });
+    }
+
+    onChangeDescription(e){
+        this.setState({
+            description: e.target.value
+        });
+    }
+
+    onChangeDateStarted(e){
+        this.setState({
+            dateStarted: e.target.value
+        });
+    }
+
+    onChangeDateCompleted(e){
+        this.setState({
+            dateCompleted: e.target.value
+        });
+    }
+
+    onSubmit(e){
+        e.preventDefault();
+       //const {history} = this.props;
+
+        let tripObject = {
+            Id: Math.floor(Math.random()*1000),
+            name: this.state.name,
+            description: this.state.description,
+            dateStarted: this.state.dateStarted,
+            dateCompleted: this.state.dateCompleted
+        }
+
+        axios.post("https://localhost:7055/api/Trips/AddTrip", tripObject).then(result => {
+            this.setState({ redirect: true }); // Trigger redirection
+            
+        })
+        .catch(error => {
+            console.error("There was an error with the request:", error);
+        })
+    }
+
     render(){
+        if (this.state.redirect) {
+            return <Navigate to="/trips" />; // Redirect to /trips after success
+        }
         return (
             <div className="trip-form" >
                 <h3>Add new trip</h3>
@@ -22,6 +80,8 @@ export class Create extends Component{
                         <input 
                           type="text" 
                           className="form-control" 
+                          value={this.state.name}
+                          onChange={this.onChangeName}
                          />
                     </div>
                     <div className="form-group">
@@ -29,6 +89,8 @@ export class Create extends Component{
                         <textarea 
 			  type="text" 
                           className="form-control"
+                          value={this.state.description}
+                          onChange={this.onChangeDescription}
                         />
                     </div>
                     <div className="row">
@@ -38,6 +100,8 @@ export class Create extends Component{
                                 <input 
                                   type="date" 
                                   className="form-control" 
+                                  value={this.state.dateStarted}
+                                  onChange={this.onChangeDateStarted}
                                 />
                             </div>
                         </div>
@@ -47,6 +111,8 @@ export class Create extends Component{
                             <input 
                                 type="date" 
                                 className="form-control" 
+                                value={this.state.dateCompleted}
+                                onChange={this.onChangeDateCompleted}
                             />
                             </div>
                         </div>
@@ -61,3 +127,5 @@ export class Create extends Component{
         )
     }
 }
+
+export default Create;
